b03014536ebd4a3fb56e962bcde247e7e1183c31
Artur Iatsko <arturyatsko@gmail.com>
2022-07-23 16:50:19 -0400

    feat(station): add bike icon to badge

--- src/components/Station.js
+++ src/components/Station.js
@@ -1,8 +1,23 @@
-import { Card, Paper } from "@mantine/core";
+import { Badge, Card, Group, Paper, Text } from "@mantine/core";
 import React from "react";
+import PedalBikeIcon from "@mui/icons-material/PedalBike";
 
 const Station = ({ station }) => {
-  return <Card>{station.name}</Card>;
+  return (
+    <Card>
+      <Text>{station.name}</Text>
+      <Group>
+        <Badge
+          size="lg"
+          leftSection={<PedalBikeIcon sx={{ alignSelf: "baseline" }} />}
+        >
+          15
+        </Badge>
+        <Badge>15</Badge>
+        <Badge>15</Badge>
+      </Group>
+    </Card>
+  );
 };
 
 export default Station;
