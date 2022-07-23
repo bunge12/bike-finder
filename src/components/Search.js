372f865fbae80a07abac78a3ecaa23512cc97f59
Artur Iatsko <arturyatsko@gmail.com>
2022-07-23 19:25:46 -0400

    feat(search): replace old inputs with quantityinputs

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,25 +8,29 @@ import {
   Text,
 } from "@mantine/core";
 import React from "react";
+import QuantityInputs from "./QuantityInputs";
 
 const Search = () => {
   return (
     <Card>
       <Grid align="center" gutter="lg">
-        <Grid.Col span={4}>
+        <Grid.Col span={3}>
           <Text>Search for</Text>
         </Grid.Col>
         <Grid.Col span={4}>
-          <NumberInput defaultValue={5}></NumberInput>
+          <QuantityInputs />
         </Grid.Col>
         <Grid.Col span={4}>
           <Text>closest</Text>
         </Grid.Col>
-        <Grid.Col span={6}>
+        <Grid.Col span={5}>
           <Text>stations with at least</Text>
         </Grid.Col>
-        <Grid.Col span={6}>
-          <NumberInput defaultValue={2}></NumberInput>
+        <Grid.Col span={4}>
+          <QuantityInputs />
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <Text>available</Text>
         </Grid.Col>
         <Grid.Col
           span={12}
