e284b053571074944e36a1877de274798fb81294
Artur Iatsko <arturyatsko@gmail.com>
2022-07-23 16:57:18 -0400

    feat(station): align icon with text

--- src/components/Station.js
+++ src/components/Station.js
@@ -1,6 +1,7 @@
 import { Badge, Card, Group, Paper, Text } from "@mantine/core";
 import React from "react";
 import PedalBikeIcon from "@mui/icons-material/PedalBike";
+import styled from "@emotion/styled";
 
 const Station = ({ station }) => {
   return (
@@ -9,7 +10,8 @@ const Station = ({ station }) => {
       <Group>
         <Badge
           size="lg"
-          leftSection={<PedalBikeIcon sx={{ alignSelf: "baseline" }} />}
+          leftSection={<PedalBikeIcon />}
+          styles={() => ({ leftSection: { alignSelf: "baseline" } })}
         >
           15
         </Badge>
