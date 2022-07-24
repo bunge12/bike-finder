7fd3e34b51d7ad1dc12b788c6819afd50076a0c3
Artur Iatsko <arturyatsko@gmail.com>
2022-07-23 22:17:48 -0400

    feat(search): restyle to fit inside modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { Button, Stack, Text } from "@mantine/core";
+import React, { useState } from "react";
+import { Button, Modal, Stack, Text } from "@mantine/core";
 import "./App.css";
 import Header from "./components/Header";
 import Station from "./components/Station";
 import sampleResponse from "./stories/sampleResponse.json";
+import Search from "./components/Search";
 
 function App() {
+  const [openedSearch, setOpenedSearch] = useState(false);
+  const [searchQuery, setSearchQuery] = useState();
   return (
     <div className="App">
       <Header />
@@ -19,10 +23,18 @@ function App() {
           return <Station key={i} station={station} />;
         })}
         <Text size="sm">Alternatively, you can</Text>
-        <Button color="bike-share" variant="outline" uppercase>
+        <Button
+          color="bike-share"
+          variant="outline"
+          uppercase
+          onClick={() => setOpenedSearch(true)}
+        >
           search for a station
         </Button>
       </Stack>
+      <Modal opened={openedSearch} onClose={() => setOpenedSearch(false)}>
+        <Search onSearch={setSearchQuery} />
+      </Modal>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -9,58 +9,46 @@ const Search = ({ onSearch }) => {
   const [quantity, setQuantity] = useState(DEFAULT_QUANTITY);
   const [item, setItem] = useState("bikes");
   return (
-    <Card>
-      <Grid align="center" gutter="lg">
-        <Grid.Col span={3}>
-          <Text>Search for</Text>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <QuantityInputs
-            onValueChange={setStations}
-            startingValue={DEFAULT_STATIONS}
-          />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Text>closest stations</Text>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Text>with at least</Text>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <QuantityInputs
-            onValueChange={setQuantity}
-            startingValue={DEFAULT_QUANTITY}
-          />
-        </Grid.Col>
-        <Grid.Col span={2}>
-          <Text>available</Text>
-        </Grid.Col>
-        <Grid.Col
-          span={12}
-          style={{ display: "flex", justifyContent: "center" }}
+    <Grid align="center" gutter="lg" justify="center">
+      <Grid.Col span={4}>
+        <Text>Search for</Text>
+      </Grid.Col>
+      <Grid.Col span={5}>
+        <QuantityInputs
+          onValueChange={setStations}
+          startingValue={DEFAULT_STATIONS}
+        />
+      </Grid.Col>
+      <Grid.Col span={12}>
+        <Text align="center">closest stations with at least</Text>
+      </Grid.Col>
+
+      <Grid.Col span={5}>
+        <QuantityInputs
+          onValueChange={setQuantity}
+          startingValue={DEFAULT_QUANTITY}
+        />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Text>available</Text>
+      </Grid.Col>
+      <Grid.Col span={12} style={{ display: "flex", justifyContent: "center" }}>
+        <Chips align="center" multiple={false} value={item} onChange={setItem}>
+          <Chip value="bikes">bikes</Chip>
+          <Chip value="e-bikes">e-bikes</Chip>
+          <Chip value="docks">docks</Chip>
+        </Chips>
+      </Grid.Col>
+      <Grid.Col span={12} style={{ display: "flex" }}>
+        <Button
+          color="bike-share"
+          style={{ flex: 1 }}
+          onClick={() => onSearch({ stations, quantity, item })}
         >
-          <Chips
-            align="center"
-            multiple={false}
-            value={item}
-            onChange={setItem}
-          >
-            <Chip value="bikes">bikes</Chip>
-            <Chip value="e-bikes">e-bikes</Chip>
-            <Chip value="docks">docks</Chip>
-          </Chips>
-        </Grid.Col>
-        <Grid.Col span={12} style={{ display: "flex" }}>
-          <Button
-            color="bike-share"
-            style={{ flex: 1 }}
-            onClick={() => onSearch({ stations, quantity, item })}
-          >
-            Search for {stations} stations with {quantity} {item}
-          </Button>
-        </Grid.Col>
-      </Grid>
-    </Card>
+          Search for {stations} stations with {quantity} {item}
+        </Button>
+      </Grid.Col>
+    </Grid>
   );
 };
 
