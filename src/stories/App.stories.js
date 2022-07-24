9fc5be7fff68a83cd42ee547aabf97d14274de38
Artur Iatsko <arturyatsko@gmail.com>
2022-07-24 10:48:17 -0400

    feat(app): add mocking to story

--- src/stories/App.stories.js
+++ src/stories/App.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { rest } from "msw";
 import App from "../App";
+import sampleResponse from "./sampleResponse.json";
 
 export default {
   title: "UI/App",
@@ -10,3 +11,13 @@ const Template = (args) => <App {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
+Primary.parameters = {
+  msw: [
+    rest.post(
+      `${process.env.REACT_APP_API_ROUTE}/api/stations`,
+      (_req, res, ctx) => {
+        return res(ctx.json(sampleResponse));
+      }
+    ),
+  ],
+};
